a4c85e7423c03203f462d9f0484c297145aad8a0
admin <admin@TungVT.local>
2022-03-02 00:46:20 +0700

    Add docker file

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /kodomoapp/tmp/pids/server.pid
+
+bundle exec rails db:create
+bundle exec rails db:migrate 
+bundle exec rails db:seed
+
+bundle exec rails s -p 3000 -b '0.0.0.0'
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
